443115d920fc280f37615d4ff096054d5f1a8d52
TomaszJa <t.jaszcz99@gmail.com>
2021-05-20 18:30:44 +0200

    Dodaj pliki projektów.

---
+++ OAST_MM1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OAST_MM1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
